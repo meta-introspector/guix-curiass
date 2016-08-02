a6849d4a45292b37e83910583464409743905e74
Mathieu Lirzin <mthl@gnu.org>
2016-08-02 17:04:00 +0200

    Add (cuirass) module.
    * src/cuirass.scm: New file.
    * Makefile.am (guilesitedir, dist_guilesite_DATA)
    (nodist_guilesite_DATA): New variables.
    (pkgmoduledir): Refer to 'guilesitedir'.
    (CLEANFILES): Add 'nodist_guilesite_DATA'.
    * bin/cuirass.in: Use (cuirass) module.
    * bin/evaluate.in: Likewise.

---
+++ src/cuirass.scm
@@ -0,0 +1,33 @@
+;;;; cuirass.scm -- Cuirass public interface.
+;;; Copyright © 2016 Mathieu Lirzin <mthl@gnu.org>
+;;;
+;;; This file is part of Cuirass.
+;;;
+;;; Cuirass is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Cuirass is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Cuirass.  If not, see <http://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+;;;
+;;; This composite module re-exports everything from the public submodules.
+;;;
+;;;; Code:
+
+(define-module (cuirass))
+
+;;; Module usages and exports need to be done at expansion time.
+(eval-when (eval load expand)
+  (let ((i (module-public-interface (current-module))))
+    (for-each (λ (m) (module-use! i (resolve-interface m)))
+              ;; Public modules.
+              '((cuirass base)
+                (cuirass database)))))
