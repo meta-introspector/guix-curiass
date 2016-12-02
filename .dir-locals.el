cbdb59af8e7a1b40d687f80e62c5892686d384d2
Mathieu Lirzin <mthl@gnu.org>
2016-12-02 15:57:06 +0100

    doc: Add Cuirass manual.
    * doc/cuirass.texi: New file.
    * doc/fdl-1.3.texi: Likewise.
    * Makefile.am (info_TEXINFOS, doc_cuirass_TEXINFOS): New variables.
    * dir-locals.el: Use the American dictionary for Texinfo mode.
    * .gitignore: Update.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,19 @@
-;; Per-directory local variables for GNU Emacs 23 and later.
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  . ((fill-column . 78)
-     (tab-width   .  8)
-
-     ;; For use with 'bug-reference-prog-mode'.
-     (bug-reference-url-format . "http://bugs.gnu.org/%s")
-     (bug-reference-bug-regexp
-      . "<https?://\\(debbugs\\|bugs\\)\\.gnu\\.org/\\([0-9]+\\)>")))
+  (fill-column . 78)
+  (tab-width . 8)
+  (bug-reference-url-format . "http://bugs.gnu.org/%s")
+  (bug-reference-bug-regexp
+   . "<https?://\\(debbugs\\|bugs\\)\\.gnu\\.org/\\([0-9]+\\)>"))
  (scheme-mode
-  .
-  ((indent-tabs-mode . nil)
-   (eval . (put 'call-with-time 'scheme-indent-function 1))
-   (eval . (put 'test-error 'scheme-indent-function 1))
-   (eval . (put 'make-parameter 'scheme-indent-function 1))
-   (eval . (put 'with-database 'scheme-indent-function 1)))))
+  (indent-tabs-mode)
+  (eval put 'call-with-time 'scheme-indent-function 1)
+  (eval put 'test-error 'scheme-indent-function 1)
+  (eval put 'make-parameter 'scheme-indent-function 1)
+  (eval put 'with-database 'scheme-indent-function 1))
+ (texinfo-mode
+  (indent-tabs-mode)
+  (fill-column . 72)
+  (ispell-local-dictionary . "american")))
