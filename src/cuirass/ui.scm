b349678589ccf5a4c6aeca89fbc0d17df696738a
Ludovic Courtès <ludo@gnu.org>
2018-01-24 14:02:53 +0100

    ui: Bump copyright year in '--version' output.
    * src/cuirass/ui.scm (show-version): Update year.

--- src/cuirass/ui.scm
+++ src/cuirass/ui.scm
@@ -38,7 +38,7 @@
   "Display version information for COMMAND."
   (simple-format #t "~a (~a) ~a~%"
                  (%program-name) %package-name %package-version)
-  (display "Copyright (C) 2016 the Cuirass authors
+  (display "Copyright (C) 2018 the Cuirass authors
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.")
