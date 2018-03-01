meta-introspector/guix-curiass

commit 593cb7be108ed97bca5371aad2e53fa8ce4817ba

http: Interpret id and nr request-parameters as numbers.
* src/cuirass/http.scm (request-parameters): Interpret id and nr parameters as
numbers.

src/cuirass/http.scm
@@ -83,7 +83,11 @@
          (map (lambda (param)
                 (match (string-split param #\=)
                   ((key param)
-                   (list (string->symbol key) param))))
+                   (let ((key-symbol (string->symbol key)))
+                     (list key-symbol ((match key-symbol
+                                        ('id string->number)
+                                        ('nr string->number)
+                                        (_ const)) param))))))
               (string-split query #\&)))))
 
 

commit dbea9790d30ff1c2e80585e9441d45ed0b740e30

http: Fix interpretation of non-numerical parameters.
Fixes a regression introduced in
593cb7be108ed97bca5371aad2e53fa8ce4817ba.

* src/cuirass/http.scm (request-parameters): Fix fallback case in
'match' form.  Previously it would return a procedure in this case, as
returned by (const param), leading to a failure down the road in
'sqlite-bind-arguments' as could be seen by running tests/http.scm.

src/cuirass/http.scm
@@ -84,10 +84,11 @@
                 (match (string-split param #\=)
                   ((key param)
                    (let ((key-symbol (string->symbol key)))
-                     (list key-symbol ((match key-symbol
-                                        ('id string->number)
-                                        ('nr string->number)
-                                        (_ const)) param))))))
+                     (list key-symbol
+                           (match key-symbol
+                             ('id (string->number param))
+                             ('nr (string->number param))
+                             (_   param)))))))
               (string-split query #\&)))))
 
 