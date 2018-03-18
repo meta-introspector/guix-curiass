meta-introspector/guix-curiass

commit f083282fd3bf813fda0b54ed33278d2d5325dfa1

http: Return build completion time as #:timestamp when completed.
* src/cuirass/http.scm (build->hydra-build): Set #:timestamp to
  #:stoptime when BUILD is finished.

src/cuirass/http.scm
@@ -35,11 +35,23 @@
   (define (bool->int bool)
     (if bool 1 0))
 
+  (define finished?
+    (bool->int
+     (not (memv (assq-ref build #:status)
+                (list (build-status scheduled)
+                      (build-status started))))))
+
   `((#:id . ,(assq-ref build #:id))
     (#:project . ,(assq-ref build #:repo-name))
     (#:jobset . ,(assq-ref build #:branch))
     (#:job . ,(assq-ref build #:job-name))
-    (#:timestamp . ,(assq-ref build #:timestamp))
+
+    ;; Hydra's API uses "timestamp" as the time of the last useful event for
+    ;; that build: evaluation or completion.
+    (#:timestamp . ,(if finished?
+                        (assq-ref build #:stoptime)
+                        (assq-ref build #:timestamp)))
+
     (#:starttime . ,(assq-ref build #:starttime))
     (#:stoptime . ,(assq-ref build #:stoptime))
     (#:derivation . ,(assq-ref build #:derivation))
@@ -50,10 +62,7 @@
     (#:busy . ,(bool->int (eqv? (build-status started)
                                 (assq-ref build #:status))))
     (#:priority . 0)
-    (#:finished . ,(bool->int
-                    (not (memv (assq-ref build #:status)
-                               (list (build-status scheduled)
-                                     (build-status started))))))
+    (#:finished . ,finished?)
     (#:buildproducts . #nil)
     (#:releasename . #nil)
     (#:buildinputs_builds . #nil)))

commit 787969c9af165113f67903173e3feb1a3e50f703

http: Correctly set #:timestamp for non-finished builds.
Fixes a regression introduced in
f083282fd3bf813fda0b54ed33278d2d5325dfa1, whereby we'd return 0 as the
timestamp for everything in /api/queue.

* src/cuirass/http.scm (build->hydra-build): Make 'finished?' a
Boolean.  Move 'bool->int' call in #:finished definition.

src/cuirass/http.scm
@@ -36,10 +36,9 @@
     (if bool 1 0))
 
   (define finished?
-    (bool->int
-     (not (memv (assq-ref build #:status)
-                (list (build-status scheduled)
-                      (build-status started))))))
+    (not (memv (assq-ref build #:status)
+               (list (build-status scheduled)
+                     (build-status started)))))
 
   `((#:id . ,(assq-ref build #:id))
     (#:project . ,(assq-ref build #:repo-name))
@@ -62,7 +61,7 @@
     (#:busy . ,(bool->int (eqv? (build-status started)
                                 (assq-ref build #:status))))
     (#:priority . 0)
-    (#:finished . ,finished?)
+    (#:finished . ,(bool->int finished?))
     (#:buildproducts . #nil)
     (#:releasename . #nil)
     (#:buildinputs_builds . #nil)))