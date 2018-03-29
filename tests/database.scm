meta-introspector/guix-curiass

commit 1bab5c4e56eb1849edc2cf0b23d433aeb2cac421

database: Simplify 'db-get-builds'.
* src/cuirass/database.scm (db-get-builds): Modify.
(db-get-build): Modify.

src/cuirass/database.scm
@@ -26,6 +26,7 @@
   #:use-module (ice-9 rdelim)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-19)
+  #:use-module (srfi srfi-26)
   #:use-module (sqlite3)
   #:export (;; Procedures.
             db-init
@@ -347,15 +348,6 @@ log file for DRV."
              (cons `(,name . ((#:path . ,path)))
                    outputs))))))
 
-(define db-build-request "\
-SELECT Builds.id, Builds.timestamp, Builds.starttime, Builds.stoptime, Builds.log, Builds.status, Builds.derivation,\
-Derivations.job_name, Derivations.system, Derivations.nix_name,\
-Specifications.repo_name, Specifications.branch \
-FROM Builds \
-INNER JOIN Derivations ON Builds.derivation = Derivations.derivation and Builds.evaluation = Derivations.evaluation \
-INNER JOIN Evaluations ON Derivations.evaluation = Evaluations.id \
-INNER JOIN Specifications ON Evaluations.specification = Specifications.repo_name")
-
 (define (db-format-build db build)
   (match build
     (#(id timestamp starttime stoptime log status derivation job-name system
@@ -374,112 +366,65 @@ INNER JOIN Specifications ON Evaluations.specification = Specifications.repo_nam
        (#:outputs    . ,(db-get-outputs db id))
        (#:branch     . ,branch)))))
 
-(define (db-get-build db id)
-  "Retrieve a build in database DB which corresponds to ID."
-  (let ((res (sqlite-exec db (string-append db-build-request
-                                            " WHERE Builds.id=")
-                          id ";")))
-    (match res
-      ((build)
-       (db-format-build db build))
-      (() #f))))
-
 (define (db-get-builds db filters)
   "Retrieve all builds in database DB which are matched by given FILTERS.
 FILTERS is an assoc list which possible keys are 'project | 'jobset | 'job |
 'system | 'nr | 'order | 'status."
 
-  (define (clauses->query+arguments clauses)
-    ;; Given CLAUSES, return two values: a SQL query string, and a list of
-    ;; arguments to bind.  Each element of CLAUSES must be either a string, or
-    ;; a (SQL ARGUMENT) tuple, where SQL is a query fragment and ARGUMENT is
-    ;; the argument to be bound for that fragment.
-    (let loop ((clauses   clauses)
-               (query     '())
-               (arguments '()))
-      (match clauses
-        (()
-         (values (string-concatenate-reverse query)
-                 (reverse arguments)))
-        (((? string? clause) . rest)
-         (loop rest
-               (cons clause query)
-               arguments))
-        ((((? string? clause) argument) . rest)
-         (loop rest
-               (cons clause query)
-               (cons argument arguments))))))
+  ;; XXX Change caller and remove
+  (define (assqx-ref filters key)
+    (if (null? filters)
+        #f
+        (match (car filters)
+         ((xkey xvalue) (if (eq? key xkey)
+                            xvalue
+                            (assqx-ref (cdr filters) key))))))
+  (let* ((order (if (eq? (assqx-ref filters 'order) 'build-id)
+                     "ASC"
+                     "DESC"))
+         (order-column-name
+          (match (assqx-ref filters 'order)
+                  (('order 'build-id) "Builds.id")
+                  (('order 'decreasing-build-id) "Builds.id")
+                  (('order 'finish-time) "Builds.stoptime")
+                  (('order 'start-time) "Builds.starttime")
+                  (('order 'submission-time) "Builds.timestamp")
+                  (_ "Builds.id")))
+         (stmt-text (format #f "\
+SELECT Builds.id, Builds.timestamp, Builds.starttime, Builds.stoptime, Builds.log, Builds.status, Builds.derivation,\
+Derivations.job_name, Derivations.system, Derivations.nix_name,\
+Specifications.repo_name, Specifications.branch \
+FROM Builds \
+INNER JOIN Derivations ON Builds.derivation = Derivations.derivation AND Builds.evaluation = Derivations.evaluation \
+INNER JOIN Evaluations ON Derivations.evaluation = Evaluations.id \
+INNER JOIN Specifications ON Evaluations.specification = Specifications.repo_name \
+WHERE (:id IS NULL OR (:id = Builds.id)) \
+AND (:project IS NULL OR (:project = Specifications.repo_name)) \
+AND (:jobset IS NULL OR (:jobset = Specifications.branch)) \
+AND (:job IS NULL OR (:job = Derivations.job_name)) \
+AND (:system IS NULL OR (:system = Derivations.system)) \
+AND (:status IS NULL OR (:status = 'done' AND Builds.status >= 0) OR (:status = 'pending' AND Builds.status < 0)) \
+ORDER BY ~a ~a LIMIT :nr;" order-column-name order))
+         (stmt (sqlite-prepare db stmt-text #:cache? #t)))
+    (sqlite-bind-arguments stmt #:id (assqx-ref filters 'id)
+                                #:project (assqx-ref filters 'project)
+                                #:jobset (assqx-ref filters 'jobset)
+                                #:job (assqx-ref filters 'job)
+                                #:system (assqx-ref filters 'system)
+                                #:status (and=> (assqx-ref filters 'status)
+                                                object->string)
+                                #:nr (match (assqx-ref filters 'nr)
+                                      (#f -1)
+                                      (x x)))
+    (map (cut db-format-build db <>)
+         (sqlite-fold-right cons '() stmt))))
 
-  (define (where-clauses filters)
-    (match (filter-map (match-lambda
-                         (('project project)
-                          (list "Specifications.repo_name=?" project))
-                         (('jobset jobset)
-                          (list "Specifications.branch=?" jobset))
-                         (('job job)
-                          (list "Derivations.job_name=?" job))
-                         (('system system)
-                          (list "Derivations.system=?" system))
-                         (('status 'done)
-                          "Builds.status >= 0")
-                         (('status 'pending)
-                          "Builds.status < 0")
-                         (_ #f))
-                       filters)
-      (()
-       '(""))
-      ((clause)
-       (list "WHERE " clause))
-      ((clause0 rest ...)
-       (cons* "WHERE " clause0
-              (fold-right (lambda (clause result)
-                            `(" AND " ,clause ,@result))
-                          '()
-                          rest)))))
-
-  (define (order-clause filters)
-    (or (any (match-lambda
-               (('order 'build-id)
-                "ORDER BY Builds.id ASC")
-               (('order 'decreasing-build-id)
-                "ORDER BY Builds.id DESC")
-               (('order 'finish-time)
-                "ORDER BY Builds.stoptime DESC")
-               (('order 'start-time)
-                "ORDER BY Builds.start DESC")
-               (('order 'submission-time)
-                "ORDER BY Builds.timestamp DESC")
-               (('order 'status+submission-time)
-                ;; With this order, builds in 'running' state (-1) appear
-                ;; before those in 'scheduled' state (-2).
-                "ORDER BY Builds.status DESC, Builds.timestamp DESC")
-               (_ #f))
-             filters)
-        "ORDER BY Builds.id DESC"))               ;default order
-
-  (define (limit-clause filters)
-    (or (any (match-lambda
-               (('nr number)
-                (list "LIMIT ?" number))
-               (_ #f))
-             filters)
-        ""))
-
-  (call-with-values
-      (lambda ()
-        (clauses->query+arguments (append (list db-build-request " ")
-                                          (where-clauses filters) '(" ")
-                                          (list (order-clause filters) " ")
-                                          (list (limit-clause filters) " "))))
-    (lambda (sql arguments)
-      (let loop ((rows    (apply %sqlite-exec db sql arguments))
-                 (outputs '()))
-        (match rows
-          (()
-           (reverse outputs))
-          ((row . rest)
-           (loop rest
-                 (cons (db-format-build db row) outputs))))))))
+(define (db-get-build db id)
+  "Retrieve a build in database DB which corresponds to ID."
+  (match (db-get-builds db `((id ,id)))
+    ((build)
+     build)
+    (() #f)))
 
 (define (db-get-stamp db spec)
   "Return a stamp corresponding to specification SPEC in database DB."

commit 8eefd24672d257e8bdfe7abe063da1d01d14d762

database: 'db-get-builds' honors 'status+submission-time' ordering again.
Fixes a regression introduced in
1bab5c4e56eb1849edc2cf0b23d433aeb2cac421 whereby the
'status+submission-time' order would no longer be honored.

As a result, /api/queue would return the queue ordered by build IDs,
making it largely useless.

* src/cuirass/database.scm (db-get-builds): Remove 'order' and rename
'order-column-name' to 'order'.  Add case for 'status+submission-time'.
* tests/database.scm ("database")["db-get-builds"]: Move below
"db-update-build-status!" test.  Add case for the
'status+submission-time' order.

src/cuirass/database.scm
@@ -460,17 +460,17 @@ Assumes that if group id stays the same the group headers stay the same."
        (let ((x-repeated-row (list->vector other-cells)))
          (collect-outputs x-builds-id x-repeated-row '() rows)))))
 
-  (let* ((order (if (eq? (assqx-ref filters 'order) 'build-id)
-                    "ASC"
-                    "DESC"))
-         (order-column-name
-          (match (assqx-ref filters 'order)
-            (('order 'build-id) "Builds.id")
-            (('order 'decreasing-build-id) "Builds.id")
-            (('order 'finish-time) "Builds.stoptime")
-            (('order 'start-time) "Builds.starttime")
-            (('order 'submission-time) "Builds.timestamp")
-            (_ "Builds.id")))
+  (let* ((order (match (assq 'order filters)
+                  (('order 'build-id) "Builds.id ASC")
+                  (('order 'decreasing-build-id) "Builds.id DESC")
+                  (('order 'finish-time) "Builds.stoptime DESC")
+                  (('order 'start-time) "Builds.starttime DESC")
+                  (('order 'submission-time) "Builds.timestamp DESC")
+                  (('order 'status+submission-time)
+                   ;; With this order, builds in 'running' state (-1) appear
+                   ;; before those in 'scheduled' state (-2).
+                   "Builds.status DESC, Builds.timestamp DESC")
+                  (_ "Builds.id DESC")))
          (stmt-text (format #f "\
 SELECT Builds.id, Outputs.name, Outputs.path, Builds.timestamp, Builds.starttime, Builds.stoptime, Builds.log, Builds.status, Builds.derivation,\
 Derivations.job_name, Derivations.system, Derivations.nix_name,\
@@ -486,7 +486,7 @@ AND (:jobset IS NULL OR (:jobset = Specifications.branch)) \
 AND (:job IS NULL OR (:job = Derivations.job_name)) \
 AND (:system IS NULL OR (:system = Derivations.system)) \
 AND (:status IS NULL OR (:status = 'done' AND Builds.status >= 0) OR (:status = 'pending' AND Builds.status < 0)) \
-ORDER BY ~a ~a, Builds.id ASC LIMIT :nr;" order-column-name order))
+ORDER BY ~a, Builds.id ASC LIMIT :nr;" order))
          (stmt (sqlite-prepare db stmt-text #:cache? #t)))
     (sqlite-bind-arguments stmt #:id (assqx-ref filters 'id)
                            #:project (assqx-ref filters 'project)

tests/database.scm
@@ -119,40 +119,6 @@ INSERT INTO Evaluations (specification, revision) VALUES (3, 3);")
       ;; This should be idempotent, see <https://bugs.gnu.org/28094>.
       (db-add-build (%db) build)))
 
-  (test-equal "db-get-builds"
-    #(((1 "/foo.drv") (2 "/bar.drv") (3 "/baz.drv")) ;ascending order
-      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;descending order
-      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;ditto
-      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;ditto
-      ((3 "/baz.drv")))                              ;nr = 1
-    (with-temporary-database db
-      ;; Populate the 'Builds', 'Derivations', 'Evaluations', and
-      ;; 'Specifications' tables in a consistent way, as expected by the
-      ;; 'db-get-builds' query.
-      (db-add-build db (make-dummy-build 1 #:drv "/foo.drv"
-                                         #:outputs `(("out" . "/foo"))))
-      (db-add-build db (make-dummy-build 2 #:drv "/bar.drv"
-                                         #:outputs `(("out" . "/bar"))))
-      (db-add-build db (make-dummy-build 3 #:drv "/baz.drv"
-                                         #:outputs `(("out" . "/baz"))))
-      (db-add-derivation db (make-dummy-derivation "/foo.drv" 1))
-      (db-add-derivation db (make-dummy-derivation "/bar.drv" 2))
-      (db-add-derivation db (make-dummy-derivation "/baz.drv" 3))
-      (db-add-evaluation db (make-dummy-eval))
-      (db-add-evaluation db (make-dummy-eval))
-      (db-add-evaluation db (make-dummy-eval))
-      (db-add-specification db example-spec)
-
-      (let ((summarize (lambda (alist)
-                         (list (assq-ref alist #:id)
-                               (assq-ref alist #:derivation)))))
-        (vector (map summarize (db-get-builds db '((nr 3) (order build-id))))
-                (map summarize (db-get-builds db '()))
-                (map summarize (db-get-builds db '((project "guix"))))
-                (map summarize (db-get-builds db '((project "guix")
-                                                   (jobset "master"))))
-                (map summarize (db-get-builds db '((nr 1))))))))
-
   (test-equal "db-update-build-status!"
     (list (build-status scheduled)
           (build-status started)
@@ -185,6 +151,46 @@ INSERT INTO Evaluations (specification, revision) VALUES (3, 3);")
                   (log     (get-status #:log)))
               (and (> start 0) (>= end start)
                    (list status0 status1 status2 log))))))))
+
+  (test-equal "db-get-builds"
+    #(((1 "/foo.drv") (2 "/bar.drv") (3 "/baz.drv")) ;ascending order
+      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;descending order
+      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;ditto
+      ((3 "/baz.drv") (2 "/bar.drv") (1 "/foo.drv")) ;ditto
+      ((3 "/baz.drv"))                               ;nr = 1
+      ((2 "/bar.drv") (1 "/foo.drv") (3 "/baz.drv"))) ;status+submission-time
+    (with-temporary-database db
+      ;; Populate the 'Builds', 'Derivations', 'Evaluations', and
+      ;; 'Specifications' tables in a consistent way, as expected by the
+      ;; 'db-get-builds' query.
+      (db-add-build db (make-dummy-build 1 #:drv "/foo.drv"
+                                         #:outputs `(("out" . "/foo"))))
+      (db-add-build db (make-dummy-build 2 #:drv "/bar.drv"
+                                         #:outputs `(("out" . "/bar"))))
+      (db-add-build db (make-dummy-build 3 #:drv "/baz.drv"
+                                         #:outputs `(("out" . "/baz"))))
+      (db-add-derivation db (make-dummy-derivation "/foo.drv" 1))
+      (db-add-derivation db (make-dummy-derivation "/bar.drv" 2))
+      (db-add-derivation db (make-dummy-derivation "/baz.drv" 3))
+      (db-add-evaluation db (make-dummy-eval))
+      (db-add-evaluation db (make-dummy-eval))
+      (db-add-evaluation db (make-dummy-eval))
+      (db-add-specification db example-spec)
+
+      (db-update-build-status! db "/bar.drv" (build-status started)
+                               #:log-file "/bar.drv.log")
+
+      (let ((summarize (lambda (alist)
+                         (list (assq-ref alist #:id)
+                               (assq-ref alist #:derivation)))))
+        (vector (map summarize (db-get-builds db '((nr 3) (order build-id))))
+                (map summarize (db-get-builds db '()))
+                (map summarize (db-get-builds db '((project "guix"))))
+                (map summarize (db-get-builds db '((project "guix")
+                                                   (jobset "master"))))
+                (map summarize (db-get-builds db '((nr 1))))
+                (map summarize
+                     (db-get-builds db '((order status+submission-time))))))))
 
   (test-assert "db-close"
     (db-close (%db)))