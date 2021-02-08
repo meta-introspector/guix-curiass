5e5850533e680fd090f7fb695d9133cb0d98b23a
Mathieu Othacehe <othacehe@gnu.org>
2021-02-08 13:31:28 +0100

    Add Zabbix command line argument.
    * bin/cuirass.in: Add "--zabbix-uri" argument.
    * src/cuirass/zabbix.scm (%zabbix-uri): Export it.

--- src/cuirass/zabbix.scm
+++ src/cuirass/zabbix.scm
@@ -25,7 +25,8 @@
   #:use-module (rnrs bytevectors)
   #:use-module (srfi srfi-11)
   #:use-module (ice-9 match)
-  #:export (zabbix-api-version
+  #:export (%zabbix-uri
+            zabbix-api-version
             zabbix-available?
             zabbix-login
             zabbix-logout
