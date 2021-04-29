95516c5b0bec3cf7c98d93bf324663d7af927bc5
Mathieu Othacehe <othacehe@gnu.org>
2021-04-29 15:31:32 +0200

    Fix html display.
    * src/cuirass/http.scm (uri-handler): Remove the old doctype.
    * src/cuirass/templates.scm (specification-edit): Fix label element
    without end tag.

--- src/cuirass/http.scm
+++ src/cuirass/http.scm
@@ -502,10 +502,7 @@ passed, only display JOBS targeting this SYSTEM."
            content-type))
      #:body
      (lambda (port)
-       (format
-        port "<!DOCTYPE html PUBLIC ~s ~s>"
-        "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd")
+       (format port "<!DOCTYPE html>")
        (sxml->xml body port))))
 
   (define* (respond-xml body #:key code)
