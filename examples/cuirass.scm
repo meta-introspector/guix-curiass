8eb67256a15e8830a1dc684a3703e70dfc5505fe
Ludovic Courtès <ludo@gnu.org>
2023-06-02 09:49:41 +0200

    examples: Add spec for Cuirass.
    * examples/cuirass.scm: New file.
    * Makefile.am (EXTRA_DIST): Add it and 'examples/hello.scm'.

---
+++ examples/cuirass.scm
@@ -0,0 +1,11 @@
+;; This spec builds Cuirass itself, which is possible because Cuirass itself
+;; is a channel, with its '.guix-channel' file.
+
+(list (specification
+       (name 'cuirass)
+       (build '(channels . (cuirass)))
+       (channels
+        (cons (channel
+               (name 'cuirass)
+               (url "https://git.savannah.gnu.org/git/guix/guix-cuirass.git"))
+              %default-channels))))
