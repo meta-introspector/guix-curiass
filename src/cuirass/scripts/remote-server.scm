meta-introspector/guix-curiass

commit bb7579acc0a1c6dc1c8f5b994593e5b19b3f8e83

remote: Remove 'zmq-socket-ready?'.
* src/cuirass/remote.scm (zmq-socket-ready?): Remove.
* src/cuirass/scripts/remote-server.scm (zmq-start-proxy): Use 'memq'
instead.
[socket-ready?]: Remove.

src/cuirass/scripts/remote-server.scm
@@ -453,11 +453,6 @@ all network interfaces."
 (define (zmq-start-proxy backend-port)
   "This procedure starts a proxy between client connections from the IPC
 frontend to the workers connected through the TCP backend."
-  (define (socket-ready? items socket)
-    (find (lambda (item)
-            (eq? (poll-item-socket item) socket))
-          items))
-
   ;; The poll loop below must not be blocked.  Print a warning message if a
   ;; loop iteration takes more than %LOOP-TIMEOUT seconds to complete.
   (define %loop-timeout 5)
@@ -481,7 +476,7 @@ frontend to the workers connected through the TCP backend."
     (let loop ()
       (let* ((items (zmq-poll* poll-items 1000))
              (start-time (current-time)))
-        (when (zmq-socket-ready? items build-socket)
+        (when (memq build-socket items)
           (match (zmq-message-receive* build-socket)
             ((worker empty rest)
              (let* ((fetch-msg (zmq-msg-init

src/cuirass/remote.scm
@@ -71,7 +71,6 @@
 
             zmq-poll*
             zmq-message-receive*
-            zmq-socket-ready?
             zmq-empty-delimiter
 
             zmq-build-request-message
@@ -388,13 +387,6 @@ retries a call to PROC."
 
 (define zmq-message-receive*
   (EINTR-safe zmq-message-receive))
-
-(define (zmq-socket-ready? items socket)
-  "Return #t if the given SOCKET is part of ITEMS, a list returned by a
-'zmq-poll' call, return #f otherwise."
-  (find (lambda (item)
-          (eq? (poll-item-socket item) socket))
-        items))
 
 (define (zmq-remote-address message)
   (zmq-message-gets message "Peer-Address"))

commit ec44dba635a711a795c3e32d6d9efbee2c3d8aad

remote-server: Correctly check the result of 'zmq-poll'.
This partially reverts bb7579acc0a1c6dc1c8f5b994593e5b19b3f8e83, which
introduced an incorrect membership test with 'memq'.

* src/cuirass/scripts/remote-server.scm (zmq-start-proxy)[socket-ready?]:
New procedure.
Use it when checking the result of 'zmq-poll*'.

src/cuirass/scripts/remote-server.scm
@@ -455,6 +455,11 @@ all network interfaces."
 (define (zmq-start-proxy backend-port)
   "This procedure starts a proxy between client connections from the IPC
 frontend to the workers connected through the TCP backend."
+  (define (socket-ready? items socket)
+    (find (lambda (item)
+            (eq? (poll-item-socket item) socket))
+          items))
+
   ;; The poll loop below must not be blocked.  Print a warning message if a
   ;; loop iteration takes more than %LOOP-TIMEOUT seconds to complete.
   (define %loop-timeout 5)
@@ -478,7 +483,7 @@ frontend to the workers connected through the TCP backend."
     (let loop ()
       (let* ((items (zmq-poll* poll-items 1000))
              (start-time (current-time)))
-        (when (memq build-socket items)
+        (when (socket-ready? items build-socket)
           (match (zmq-message-receive* build-socket)
             ((worker empty rest)
              (let* ((command (bv->string (zmq-message-content rest)))