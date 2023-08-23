meta-introspector/guix-curiass

commit 0dbd460cf1b40f6450e4b4da93ec1a719ebaafa8

remote-worker: Sleep only after a 'no-build response.
* src/cuirass/scripts/remote-worker.scm (run-command): Move 'no-build
clause to...
(start-worker): ... here.  Sleep only after 'no-build'.

src/cuirass/scripts/remote-worker.scm
@@ -268,11 +268,7 @@ command.  REPLY is a procedure that can be used to reply to this server."
                 #:reply reply
                 #:worker worker
                 #:timeout timeout
-                #:max-silent max-silent))
-    (('no-build)
-     (log-info (G_ "~a: no available build.")
-               (worker-name worker))
-     #t)))
+                #:max-silent max-silent))))
 
 (define (spawn-worker-ping worker server)
   "Spawn a thread that periodically pings SERVER."
@@ -378,6 +374,10 @@ and executing them.  The worker can reply on the same socket."
                    ((? unspecified?)              ;server reconnect
                     (log-info (G_ "~a: received a bootstrap message.")
                               (worker-name wrk)))
+                   (('no-build)
+                    (log-info (G_ "~a: no available build.")
+                              (worker-name worker))
+                    (sleep (%request-period)))
                    (command
                     (log-debug (G_ "~a: received command: ~s")
                                (worker-name wrk) command)
@@ -385,7 +385,6 @@ and executing them.  The worker can reply on the same socket."
                                  #:reply (reply socket)
                                  #:worker worker)))))
 
-           (sleep (%request-period))
            (loop)))))))
 
 (define (worker-management-thunk channel systems)

commit dfd1ff4582f23bea282d6bee91c09f756a3b262b

remote-worker: Sleep after reporting low disk space.
Fixes a regression introduced in
0dbd460cf1b40f6450e4b4da93ec1a719ebaafa8 whereby 'cuirass remote-worker'
would keep spinning and displaying "warning: low disk space" when that
condition is met.

* src/cuirass/scripts/remote-worker.scm (start-worker): Call 'sleep' on
low disk space.

src/cuirass/scripts/remote-worker.scm
@@ -366,7 +366,9 @@ and executing them.  The worker can reply on the same socket."
          (spawn-worker-ping worker server)
          (let loop ()
            (if (low-disk-space?)
-               (log-info (G_ "warning: low disk space, doing nothing"))
+               (begin
+                 (log-info (G_ "warning: low disk space, doing nothing"))
+                 (sleep (%request-period)))
                (begin
                  (log-info (G_ "~a: request work.") (worker-name wrk))
                  (request-work socket worker)