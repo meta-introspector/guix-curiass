meta-introspector/guix-curiass

commit c445d2d642765f9589313e9ccdd826ef9bf71432

database: Use records instead of alists.
Instead of using alists, internal code now manipulates records.  Strong
typing makes the code easier to understand and to check for correctness.

* src/cuirass/database.scm (<build>, <build-product>, <job>)
(<output>): New record types.
(db-add-output): Keep a single argument, expect an <output> record.
(db-add-build-product): Expect a <build> record and return its ID on
success.
(db-add-build-product, db-get-output, db-get-outputs)
(db-get-build-percentages): Use record accessors and constructors.
(db-add-job): Rename to...
(db-add-job-for-build): ... this.  Expect records.
(db-get-jobs): Likewise.  Augment query to include 'evaluation'.
(<evaluation>): New record type.
(db-get-build-dependencies/derivation, build-dependencies/id): New
procedures.
(db-register-builds): Change signature and adjust for records.
(db-get-last-status): Produce records.
(db-get-builds-by-search, db-get-builds): Likewise.
(<checkout>): New record type.
(db-get-checkouts, db-get-latest-checkout): Produce them.
(parse-evaluation): Likewise.
(<build-summary>, <evaluation-summary>): New record types.
(db-get-latest-evaluations, db-get-evaluation-summary): Produce them.
(db-get-evaluations-absolute-summary): Adjust for records.
(<dashboard>): New record type.
(db-get-dashboard): Produce it.
* src/cuirass/http.scm (build->hydra-build): Expect records.
(evaluation->json-object): Likewise.
(evaluation-html-page): Likewise.
(url-handler): Likewise.
* src/cuirass/notification.scm (build-weather-text)
(build-details-url, notification-subject, notification-text): Likewise.
* src/cuirass/rss.scm (build-details-url, build->rss-item): Likewise.
* src/cuirass/scripts/evaluate.scm (checkouts->channel-instances):
Likewise.
(user-alists->builds): New procedure.
(inferior-evaluation): Use it.
* src/cuirass/templates.scm (specifications-table, build-details)
(input-changes, evaluation-badges, evaluation-info-table)

src/cuirass/http.scm
@@ -102,41 +102,52 @@
     (if bool 1 0))
 
   (define finished?
-    (>= (assq-ref build #:status) 0))
+    (>= (build-current-status build) 0))
 
-  `((#:id . ,(assq-ref build #:id))
-    (#:evaluation . ,(assq-ref build #:eval-id))
-    (#:jobset . ,(assq-ref build #:specification))
-    (#:job . ,(assq-ref build #:job-name))
+  `((#:id . ,(build-id build))
+    (#:evaluation . ,(build-evaluation-id build))
+    (#:jobset . ,(build-specification-name build))
+    (#:job . ,(build-job-name build))
 
     ;; Hydra's API uses "timestamp" as the time of the last useful event for
     ;; that build: evaluation or completion.
     (#:timestamp . ,(if finished?
-                        (assq-ref build #:stoptime)
-                        (assq-ref build #:timestamp)))
+                        (build-completion-time build)
+                        (build-creation-time build)))
 
-    (#:starttime . ,(assq-ref build #:starttime))
-    (#:stoptime . ,(assq-ref build #:stoptime))
-    (#:derivation . ,(assq-ref build #:derivation))
-    (#:buildoutputs . ,(assq-ref build #:outputs))
-    (#:system . ,(assq-ref build #:system))
-    (#:nixname . ,(assq-ref build #:nix-name))
-    (#:buildstatus . ,(assq-ref build #:status))
-    (#:weather . ,(assq-ref build #:weather))
+    (#:starttime . ,(build-start-time build))
+    (#:stoptime . ,(build-completion-time build))
+    (#:derivation . ,(build-derivation build))
+    (#:buildoutputs . ,(map (lambda (output)
+                              (list (output-name output)
+                                    (cons "path"
+                                          (output-item output))))
+                            (build-outputs build)))
+    (#:system . ,(build-system build))
+    (#:nixname . ,(build-nix-name build))
+    (#:buildstatus . ,(build-current-status build))
+    (#:weather . ,(build-current-weather build))
     (#:busy . ,(bool->int (eqv? (build-status started)
-                                (assq-ref build #:status))))
-    (#:priority . ,(assq-ref build #:priority))
+                                (build-current-status build))))
+    (#:priority . ,(build-priority build))
     (#:finished . ,(bool->int finished?))
-    (#:buildproducts . ,(list->vector
-                         (assq-ref build #:buildproducts)))))
+    (#:buildproducts . ,(list->vector (build-products build)))))
 
 (define (evaluation->json-object evaluation)
   "Turn EVALUATION into a representation suitable for 'json->scm'."
-  ;; XXX: Since #:checkouts is a list of alists, we must turn it into a vector
-  ;; so that 'json->scm' converts it to a JSON array.
-  `(,@(alist-delete #:checkouts evaluation eq?)
-    (#:checkouts . ,(list->vector
-                     (assq-ref evaluation #:checkouts)))))
+  `((#:id . ,(evaluation-id evaluation))
+    (#:specification . ,(evaluation-specification-name evaluation))
+    (#:status . ,(evaluation-current-status evaluation))
+    (#:timestamp . ,(evaluation-completion-time evaluation))
+    (#:checkouttime . ,(evaluation-checkout-time evaluation))
+    (#:evaltime . ,(evaluation-start-time evaluation))
+    (#:checkouts
+     . ,(list->vector
+         (map (lambda (checkout)
+                `((#:commit . ,(checkout-commit checkout))
+                  (#:channel . ,(checkout-channel checkout))
+                  (#:directory . ,(checkout-directory checkout))))
+              (evaluation-checkouts evaluation))))))
 
 (define (specification->json-object spec)
   "Turn SPEC into a representation suitable for 'json->scm'."
@@ -243,7 +254,7 @@ Hydra format."
                                border-high-time border-low-time
                                border-high-id border-low-id)
   "Return the HTML page representing EVALUATION."
-  (define id             (assq-ref evaluation #:id))
+  (define id             (evaluation-summary-id evaluation))
   (define builds-id-max  (db-get-builds-max id status))
   (define builds-id-min  (db-get-builds-min id status))
   (define specification  (db-get-evaluation-specification id))
@@ -252,8 +263,9 @@ Hydra format."
   (define checkouts      (latest-checkouts specification* id))
 
   (define builds
-    (vector->list
-     (handle-builds-request
+    (with-time-logging
+     "builds request for evaluation page"
+     (db-get-builds
       `((evaluation . ,id)
         (status . ,(and=> status string->symbol))
         ,@(if paginate?
@@ -366,7 +378,7 @@ Hydra format."
      (machine-status name workers
                      (map (lambda (worker)
                             (filter (lambda (build)
-                                      (string=? (assq-ref build #:worker)
+                                      (string=? (build-worker build)
                                                 (worker-name worker)))
                                     builds))
                           workers)
@@ -452,7 +464,7 @@ passed, only display JOBS targeting this SYSTEM."
                (car systems)))
          (dashboard (db-get-dashboard dashboard-id))
          (names (and dashboard
-                     (assq-ref dashboard #:jobs)))
+                     (dashboard-job-ids dashboard)))
          (prev (db-get-previous-eval evaluation-id))
          (next (db-get-next-eval evaluation-id)))
     (html-page
@@ -612,6 +624,12 @@ passed, only display JOBS targeting this SYSTEM."
              #:body (string-append "Resource not found: "
                                    resource_name)))
 
+  (define (job->alist job)
+    ;; Convert JOB to an alist representation suitable for JSON conversion.
+    `((build . ,(job-build-id job))
+      (status . ,(job-status job))
+      (name . ,(job-name job))))
+
   (log-info "~a ~a" (request-method request)
             (uri-path (request-uri request)))
 
@@ -681,7 +699,7 @@ passed, only display JOBS targeting this SYSTEM."
 
     (('GET "admin" "evaluation" id "cancel")
      (let* ((eval (db-get-evaluation id))
-            (specification (assq-ref eval #:specification)))
+            (specification (evaluation-specification-name eval)))
        (db-cancel-pending-builds! (string->number id))
        (respond
         (build-response
@@ -693,7 +711,7 @@ passed, only display JOBS targeting this SYSTEM."
 
     (('GET "admin" "evaluation" id "restart")
      (let* ((eval (db-get-evaluation id))
-            (specification (assq-ref eval #:specification)))
+            (specification (evaluation-specification-name eval)))
        (db-restart-evaluation! (string->number id))
        (respond
         (build-response
@@ -705,7 +723,7 @@ passed, only display JOBS targeting this SYSTEM."
 
     (('GET "admin" "evaluation" id "retry")
      (let* ((eval (db-get-evaluation id))
-            (specification (assq-ref eval #:specification)))
+            (specification (evaluation-specification-name eval)))
        (db-retry-evaluation! (string->number id))
        (respond
         (build-response
@@ -747,32 +765,33 @@ passed, only display JOBS targeting this SYSTEM."
            (respond-build-not-found id))))
     (('GET "build" (= string->number id) "details")
      (let* ((build (and id (db-get-build id)))
-            (products (and build (assoc-ref build #:buildproducts)))
+            (products (and build (build-products build)))
             (dependencies
              (and build
                   (db-get-builds
-                   `((ids . ,(assoc-ref build #:builddependencies))))))
+                   `((ids . ,(build-dependencies/id build))))))
             (history
-             (db-get-builds
-              `((jobset . ,(assq-ref build #:specification))
-                (job . ,(assq-ref build #:job-name))
-                (oldevaluation . ,(assq-ref build #:eval-id))
-                (status . done)
-                (order . evaluation)
-                (nr . 10)))))
+             (and build
+                  (db-get-builds
+                   `((jobset . ,(build-specification-name build))
+                     (job . ,(build-job-name build))
+                     (oldevaluation . ,(build-evaluation-id build))
+                     (status . done)
+                     (order . evaluation)
+                     (nr . 10))))))
        (if build
            (respond-html
             (html-page
              (string-append "Build " (number->string id))
              (build-details build dependencies products history)
-             `(((#:name . ,(assq-ref build #:specification))
+             `(((#:name . ,(build-specification-name build))
                 (#:link
                  . ,(string-append "/jobset/"
-                                   (assq-ref build #:specification)))))))
+                                   (build-specification-name build)))))))
            (respond-build-not-found id))))
     (('GET "build" (= string->number id) "log" "raw")
      (let* ((build (and id (db-get-build id)))
-            (log   (and build (assq-ref build #:log))))
+            (log   (and build (build-log build))))
        (if (and log (file-exists? log))
            (respond-compressed-file log)
            (respond-not-found (uri->string (request-uri request))))))
@@ -780,11 +799,13 @@ passed, only display JOBS targeting this SYSTEM."
      (let ((output (db-get-output
                     (string-append (%store-prefix) "/" id))))
        (if output
-           (let ((build (db-get-build (assq-ref output #:derivation))))
+           (let ((build (db-get-build (output-derivation output))))
              (respond-json
               (object->json-string
-               (append output
-                       `((#:build . ,(or build #nil)))))))
+               `((#:name . ,(output-name output))
+                 (#:derivation . ,(output-derivation output))
+                 (#:build . ,(or (and=> build build->hydra-build)
+                                 #nil))))))
            (respond-output-not-found id))))
     (('GET "api" "jobs")
      (let* ((params (request-parameters request))
@@ -793,11 +814,12 @@ passed, only display JOBS targeting this SYSTEM."
            (respond-json
             (object->json-string
              (list->vector
-              (db-get-jobs eval-id
-                           `((names
-                              . ,(and=> (assq-ref params 'names)
-                                        (cut string-split <> #\,)))
-                             ,@params)))))
+              (map job->alist
+                   (db-get-jobs eval-id
+                                `((names
+                                   . ,(and=> (assq-ref params 'names)
+                                             (cut string-split <> #\,)))
+                                  ,@params))))))
            (respond-json-with-error 500 "Parameter not defined!"))))
     (('GET "api" "jobs" "history")
      (let* ((params (request-parameters request))
@@ -846,7 +868,7 @@ passed, only display JOBS targeting this SYSTEM."
            (respond-json-with-error 500 "Parameter not defined!"))))
     (('GET "api" "evaluations")
      (let* ((params (request-parameters request))
-            (spec (assq-ref params 'spec)) ;optional
+            (spec (assq-ref params 'spec))        ;optional
             ;; 'nr parameter is mandatory to limit query size.
             (nr (assq-ref params 'nr)))
        (if nr
@@ -900,11 +922,7 @@ passed, only display JOBS targeting this SYSTEM."
                        (db-get-specifications)
                        evals
                        (db-get-evaluations-absolute-summary
-                        (map (lambda (e)
-                               `((#:id . ,(assq-ref
-                                           (assq-ref e #:evaluation)
-                                           #:id))))
-                             evals))
+                        (map evaluation-id evals))
                        ;; Get all the latest evaluations, regardless of their
                        ;; status.
                        (db-get-latest-evaluations #:status #f)))
@@ -912,13 +930,12 @@ passed, only display JOBS targeting this SYSTEM."
     (('GET "dashboard" id)
      (let ((dashboard (db-get-dashboard id)))
        (if dashboard
-           (let* ((spec (assq-ref dashboard #:specification))
+           (let* ((spec (dashboard-specification-name dashboard))
                   (evaluations (db-get-latest-evaluations))
                   (evaluation
                    (any (lambda (eval)
-                          (and (string=? (assq-ref eval #:specification)
-                                         spec)
-                               (assq-ref eval #:evaluation)))
+                          (string=? (evaluation-specification-name eval)
+                                    spec))
                         evaluations))
                   (uri
                    (string->uri-reference
@@ -1047,8 +1064,9 @@ passed, only display JOBS targeting this SYSTEM."
              "Search results"
              (build-search-results-table
               query
-              (vector->list
-               (handle-builds-search-request
+              (with-time-logging
+               "job search request"
+               (db-get-builds-by-search
                 `((query . ,query)
                   (nr . ,%page-size)
                   (order . finish-time+build-id)
@@ -1144,9 +1162,8 @@ passed, only display JOBS targeting this SYSTEM."
        "Workers status"
        (let* ((workers (db-get-workers))
               (builds (db-worker-current-builds))
-              (builds*
-               (db-get-build-percentages builds)))
-         (workers-status workers builds*))
+              (percentages (db-get-build-percentages builds)))
+         (workers-status workers builds percentages))
        '())))
 
     (('GET "metrics")

tests/http.scm
@@ -122,29 +122,35 @@
 
   (test-assert "fill-db"
     (let* ((build1
-            `((#:derivation . "/gnu/store/fake.drv")
-              (#:eval-id . 1)
-              (#:job-name . "fake-job")
-              (#:system . "x86_64-linux")
-              (#:nix-name . "fake-1.0")
-              (#:log . "unused so far")
-              (#:status . ,(build-status succeeded))
-              (#:outputs . (("out" . "/gnu/store/fake-1.0")))
-              (#:timestamp . 1501347493)
-              (#:starttime . 1501347493)
-              (#:stoptime . 1501347493)))
+            (build (derivation "/gnu/store/fake.drv")
+                   (evaluation-id 1)
+                   (specification-name "guix")
+                   (job-name "fake-job")
+                   (system "x86_64-linux")
+                   (nix-name "fake-1.0")
+                   (log "unused so far")
+                   (status (build-status succeeded))
+                   (outputs
+                    (list (output
+                           (item "/gnu/store/fake-1.0")
+                           (derivation derivation))))
+                   (creation-time 1501347493)
+                   (start-time 1501347493)
+                   (completion-time 1501347493)))
            (build2
-            `((#:derivation . "/gnu/store/fake2.drv")
-              (#:eval-id . 1)
-              (#:job-name . "fake-job")
-              (#:system . "x86_64-linux")
-              (#:nix-name . "fake-2.0")
-              (#:log . "unused so far")
-              (#:status . ,(build-status scheduled))
-              (#:outputs . (("out" . "/gnu/store/fake-2.0")))
-              (#:timestamp . 1501347493)
-              (#:starttime . 0)
-              (#:stoptime . 0)))
+            (build (derivation "/gnu/store/fake2.drv")
+                   (evaluation-id 1)
+                   (specification-name "guix")
+                   (job-name "fake-job")
+                   (system "x86_64-linux")
+                   (nix-name "fake-2.0")
+                   (log "unused so far")
+                   (status (build-status scheduled))
+                   (outputs
+                    (list (output
+                           (item "/gnu/store/fake-2.0")
+                           (derivation derivation))))
+                   (creation-time 1501347493)))
            (spec
             (specification
              (name "guix")

src/cuirass/base.scm
@@ -358,7 +358,7 @@ build products."
   (let* ((build (db-get-build drv))
          (spec  (and build
                      (db-get-specification
-                      (assq-ref build #:specification)))))
+                      (build-specification-name build)))))
     (when (and spec build)
       (create-build-outputs build
                             (specification-build-outputs spec))))
@@ -510,8 +510,8 @@ updating the database accordingly."
 
 (define (build-derivation=? build1 build2)
   "Return true if BUILD1 and BUILD2 correspond to the same derivation."
-  (string=? (assq-ref build1 #:derivation)
-            (assq-ref build2 #:derivation)))
+  (string=? (build-derivation build1)
+            (build-derivation build2)))
 
 (define (clear-build-queue)
   "Reset the status of builds in the database that are marked as \"started\".
@@ -540,19 +540,21 @@ started)."
         (spawn-builds store valid))
       (log-info "done with restarted builds"))))
 
-(define (create-build-outputs build build-outputs)
-  "Given BUILDS a list of built derivations, save the build products described
-by BUILD-OUTPUTS."
+(define (create-build-outputs build outputs)
+  "Given BUILDS, a list of <build> records, save the build products described by
+OUTPUTS, a list of <build-output> records."
   (define (build-has-products? job-regex)
-    (let ((job-name (assq-ref build #:job-name)))
+    (let ((job-name (build-job-name build)))
       (string-match job-regex job-name)))
 
   (define* (find-product build build-output)
-    (let* ((outputs (assq-ref build #:outputs))
+    (let* ((outputs (build-outputs build))
            (output (build-output-output build-output))
            (path (build-output-path build-output))
-           (root (and=> (assoc-ref outputs output)
-                        (cut assq-ref <> #:path))))
+           (root (and=> (find (lambda (o)
+                                (string=? (output-name o) output))
+                              outputs)
+                        output-item)))
       (and root
            (if (string=? path "")
                root
@@ -562,19 +564,19 @@ by BUILD-OUTPUTS."
     (stat:size (stat file)))
 
   (for-each (lambda (build-output)
-              (let ((product (and (build-has-products?
-                                   (build-output-job build-output))
-                                  (find-product build build-output))))
-                (when (and product (file-exists? product))
-                  (log-info "Adding build product ~a" product)
+              (let ((file (and (build-has-products?
+                                (build-output-job build-output))
+                               (find-product build build-output))))
+                (when (and file (file-exists? file))
+                  (log-info "Adding build product ~a" file)
                   (db-add-build-product
-                   `((#:build . ,(assq-ref build #:id))
-                     (#:type . ,(build-output-type build-output))
-                     (#:file-size . ,(file-size product))
-                     ;; TODO: Implement it.
-                     (#:checksum . "")
-                     (#:path . ,product))))))
-            build-outputs))
+                   (build-product
+                    (build-id (build-id build))
+                    (type (build-output-type build-output))
+                    (file file)
+                    (file-size (file-size file))
+                    (checksum ""))))))            ;TODO: Implement it.
+            outputs))
 
 (define (build-packages store eval-id)
   "Build JOBS and return a list of Build results."
@@ -582,7 +584,7 @@ by BUILD-OUTPUTS."
     (db-get-builds `((evaluation . ,eval-id))))
 
   (define derivations
-    (map (cut assq-ref <> #:derivation) builds))
+    (map build-derivation builds))
 
   ;; Register a GC root for each derivation so that they are not garbage
   ;; collected before getting built.
@@ -597,15 +599,12 @@ by BUILD-OUTPUTS."
     (spawn-builds store derivations)
 
     (let* ((results (filter-map (cut db-get-build <>) derivations))
-           (status (map (cut assq-ref <> #:status) results))
+           (status (map build-current-status results))
            (success (count (lambda (status)
                              (= status (build-status succeeded)))
                            status))
-           (outputs (map (cut assq-ref <> #:outputs) results))
-           (outs (append-map (match-lambda
-                               (((_ (#:path . (? string? outputs))) ...)
-                                outputs))
-                             outputs))
+           (outputs (map build-outputs results))
+           (outs (append-map build-output-path outputs))
            (fail (- (length derivations) success)))
 
       (log-info "outputs:\n~a" (string-join outs "\n"))

src/cuirass/notification.scm
@@ -95,7 +95,7 @@
 
 (define (build-weather-text build)
   "Return the build weather string."
-  (let ((weather (assq-ref build #:weather)))
+  (let ((weather (build-current-weather build)))
     (cond
      ((= weather weather-success)
       "fixed")
@@ -104,14 +104,14 @@
 
 (define (build-details-url build)
   "Return the build details URL for BUILD."
-  (let ((id (assq-ref build #:id))
+  (let ((id (build-id build))
         (url (or (%cuirass-url) "")))
     (string-append url "/build/" (number->string id) "/details")))
 
 (define (notification-subject build)
   "Return the subject for the given NOTIFICATION."
-  (let* ((job-name (assq-ref build #:job-name))
-         (specification (assq-ref build #:specification))
+  (let* ((job-name (build-job-name build))
+         (specification (build-specification-name build))
          (weather-text (build-weather-text build)))
     (format #f "Build ~a on ~a is ~a."
             job-name specification weather-text)))
@@ -119,8 +119,8 @@
 (define (notification-text build)
   "Return the text for the given NOTIFICATION."
   (let* ((url (build-details-url build))
-         (job-name (assq-ref build #:job-name))
-         (specification (assq-ref build #:specification))
+         (job-name (build-job-name build))
+         (specification (build-specification-name build))
          (weather-text (build-weather-text build)))
     (format #f "The build ~a for specification ~a is ~a. You can find \
 the detailed information about this build here: ~a."

src/cuirass/rss.scm
@@ -130,23 +130,23 @@ list ATTRS and the child nodes in BODY."
 
 (define (build-details-url build)
   "Return the build details URL for BUILD."
-  (let ((id (assq-ref build #:id))
+  (let ((id (build-id build))
         (url (or (%cuirass-url) "")))
     (string-append url "/build/" (number->string id) "/details")))
 
 (define* (build->rss-item build)
   "Convert BUILD into an RSS <item> node."
   (let* ((url (build-details-url build))
-         (id (assq-ref build #:id))
-         (job-name (assq-ref build #:job-name))
-         (specification (assq-ref build #:specification))
-         (weather  (assq-ref build #:weather))
+         (id (build-id build))
+         (job-name (build-job-name build))
+         (specification (build-specification-name build))
+         (weather (build-current-weather build))
          (weather-text (cond
                         ((= weather (build-weather new-success))
                          "fixed")
                         ((= weather (build-weather new-failure))
                          "broken")))
-         (stoptime (assq-ref build #:stoptime)))
+         (stoptime (build-completion-time build)))
     `(item
       (guid ,url)
       (title

commit 797b26a4ef80dce278481b1cc3391791ef2cbbaf

http: Fix “/api/jobs/history” endpoint.
Fixes a bug introduced in c445d2d642765f9589313e9ccdd826ef9bf71432 where
<checkout> records would end up being passed to ‘scm->json’.

* src/cuirass/http.scm (jobs-history->json-object): Pass checkouts
through ‘checkout->json-object’.
* tests/http.scm ("/api/jobs/history"): New test.

src/cuirass/http.scm
@@ -221,7 +221,8 @@ a <checkout> record."
     (map (lambda (eval)
            `((evaluation . ,(assq-ref eval 'evaluation))
              (checkouts . ,(list->vector
-                            (assq-ref eval 'checkouts)))
+                            (map checkout->json-object
+                                 (assq-ref eval 'checkouts))))
              (jobs . ,(list->vector
                        (assq-ref eval 'jobs)))))
          history))))

tests/http.scm
@@ -123,7 +123,7 @@
                    (completion-time 1501347493)))
            (build2
             (build (derivation "/gnu/store/fake2.drv")
-                   (evaluation-id 1)
+                   (evaluation-id 2)
                    (specification-name "guix")
                    (job-name "fake-job")
                    (system "x86_64-linux")
@@ -264,6 +264,24 @@
     (json-string->scm
      (utf8->string
       (http-get-body (test-cuirass-uri "/api/evaluations?nr=1")))))
+
+  (test-equal "/api/jobs/history"
+    '#((("jobs" . #((("status" . 0) ("build" . 1) ("name" . "fake-job"))))
+        ("checkouts" . #((("directory" . "dir1") ("channel" . "guix")
+                          ("commit" . "fakesha1"))
+                         (("directory" . "dir2") ("channel" . "packages")
+                          ("commit" . "fakesha3"))))
+        ("evaluation" . 1)))
+    (begin
+      (db-register-builds (list (db-get-build "/gnu/store/fake.drv"))
+                          (db-get-specification "guix"))
+      (db-set-evaluation-status 1 (evaluation-status succeeded))
+      (db-update-build-status! "/gnu/store/fake.drv"
+                               (build-status succeeded))
+      (json-string->scm
+       (utf8->string
+        (http-get-body
+         (test-cuirass-uri "/api/jobs/history?spec=guix&names=fake-job&nr=10"))))))
 
   (test-assert "db-close"
     (begin